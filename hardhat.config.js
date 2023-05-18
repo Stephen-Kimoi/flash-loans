5c228309d9a7120f424668216a1206b14c548452
Steve Kimoi <moenisteve@gmail.com>
2023-05-18 07:08:13 +0300

    tailoring hardhat config to mainnet forking

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_API_KEY,
+      },
+    },
+  },
 };
